5ee853dabc9ce6ac00dcdfb188573c7bc0f5e9d9
Russell Goldman <russellgoldman1@gmail.com>
2021-03-13 17:08:26 -0500

    Lexical tests

---
+++ src/modules/lexical.py
@@ -0,0 +1,10 @@
+from typing import List, Set, Dict, Tuple, Optional
+from enum import Enum
+
+class Lexical:
+    def __init__(self, source):
+        self.source = source
+
+    # def get_next_token():
+
+    # use Bison for lexical analysis
