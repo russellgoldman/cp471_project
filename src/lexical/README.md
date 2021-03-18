94e940d78fe35e3101ee7ae8f0992f4378d5f0ee
Zach Merrill <zemerrill@gmail.com>
2021-03-18 10:10:35 -0400

    Expanded testing and included readme

---
+++ src/lexical/README.md
@@ -0,0 +1,13 @@
+# EasyLang Lexical Analyzer
+
+## Testing
+Discover & run all tests
+```bash
+python3 -m unittest discover
+```
+> Note: filenames must be formatted like `test_*.py` to be discovered. Other naming schemes will be ignored.
+
+Or run a single test script
+```bash
+python3 ./test_number.py
+```
---
+++ src/lexical/test_string.py
@@ -0,0 +1,33 @@
+import unittest
+from lexer import Lexer
+
+# testing correct lexeme classification of the Number token
+class TestStringToken(unittest.TestCase):
+    def test_string_operators(self):
+        source = "String str3 = str1 + str2;"
+
+        expected_token = [
+            "('STRING', 'String')",
+            "('ID', 'str3')",
+            "('OPERATOR', '=')",
+            "('ID', 'str1')",
+            "('OPERATOR', '+')",
+            "('ID', 'str2')",
+            "('SEPARATOR', ';')"
+        ]
+        
+        l = Lexer()
+        l.build(source)
+        given_token = l.get_next_token()
+        token_num = 0
+
+        while given_token is not None:
+            self.assertEqual(str(given_token), expected_token[token_num], 
+                "Token {number} should be {expected}".format(number=token_num, expected=expected_token[token_num]))
+                
+            given_token = l.get_next_token()
+            token_num = token_num + 1
+
+
+if __name__ == '__main__':
+    unittest.main()
