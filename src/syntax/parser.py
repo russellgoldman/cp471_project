8ce089d73acb3d1c8b109712ef1129bb7879d348
Zach Merrill <zemerrill@gmail.com>
2021-04-01 16:49:39 -0400

    Parser class with basic functions

--- src/syntax/parser.py
+++ src/syntax/parser.py
@@ -1,17 +1,14 @@
 # Ref: https://ply.readthedocs.io/en/latest/ply.html
 import ply.yacc as yacc
-import grammar_rules_simple
+import grammar_rules_simple as grammar_rules
 from lexer import Lexer
 
 class Parser(object):
     # Build the parser using the grammar rules module
-    def build(self, data, **kwargs):
-        self.lexer = Lexer().build(data)
-        self.parser = yacc.yacc(module=grammar_rules_simple, **kwargs)
+    def build(self, **kwargs):
+        self.lexer = Lexer()
+        self.lexer.build()
+        self.parser = yacc.yacc(module=grammar_rules, **kwargs)
 
-        s = input(data)
-        result = self.parser.parse(s, lexer = self.lexer)
-        
-
-p = Parser()
-p.build('Number num = 10;')
+    def parse(self, data):
+        return self.parser.parse(data, lexer = self.lexer)
