0d3d87f0657641cfa8a985bdd5f00128075552db
Russell Goldman <russellgoldman1@gmail.com>
2021-04-14 23:35:23 -0400

    fpadd complete

--- src/intermediate/fpadd.py
+++ src/intermediate/fpadd.py
@@ -4,19 +4,17 @@ https://llvmlite.readthedocs.io/en/latest/user-guide/ir/examples.html
 This file demonstrates a trivial function "fpadd" returning the sum of
 two floating-point numbers.
 """
-from llvmlite import ir, binding
+from llvmlite import ir
+from generator import create_execution_engine, compile_ir
+from ctypes import CFUNCTYPE, c_double
 
-def generate_example():
-    # Create some useful types
+def generate_assembly():
     double = ir.DoubleType()
     fnty = ir.FunctionType(double, (double, double))
 
-    # Create an empty module...
     module = ir.Module(name=__file__)
-    # and declare a function named "fpadd" inside it
     func = ir.Function(module, fnty, name="fpadd")
 
-    # Now implement the function
     block = func.append_basic_block(name="entry")
     builder = ir.IRBuilder(block)
     a, b = func.args
@@ -24,3 +22,15 @@ def generate_example():
     builder.ret(result)
 
     return str(module)
+
+def run_assembly():
+    engine = create_execution_engine()
+    mod = compile_ir(engine, generate_assembly())
+
+    func_ptr = engine.get_function_address("fpadd")
+    cfunc = CFUNCTYPE(c_double, c_double, c_double)(func_ptr)
+    res = cfunc(10.0, 20.0)
+
+    return res
+
+print(run_assembly())
