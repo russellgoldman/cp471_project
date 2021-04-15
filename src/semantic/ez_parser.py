99e41409c7f1a61a64a254a66643555e32c599d9
Zach Merrill <zemerrill@gmail.com>
2021-04-15 09:51:22 -0400

    Rename for clarity

--- src/semantic/ez_parser.py
+++ src/semantic/ez_parser.py
@@ -1,6 +1,6 @@
 # Ref: https://ply.readthedocs.io/en/latest/ply.html
 import ply.yacc as yacc
-import grammar_rules
+import grammar_rules_ast
 from lexer import Lexer
 
 class Parser(object):
@@ -8,7 +8,7 @@ class Parser(object):
     def build(self, **kwargs):
         self.lexer = Lexer()
         self.lexer.build()
-        self.parser = yacc.yacc(module=grammar_rules, **kwargs)
+        self.parser = yacc.yacc(module=grammar_rules_ast, **kwargs)
 
     def parse(self, data, debug=False):
         return self.parser.parse(data, debug=debug, lexer = self.lexer)
