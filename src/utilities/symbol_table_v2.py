599932c967bb10b1f304d565de91a2625cfa3bf5
Russell Goldman <russellgoldman1@gmail.com>
2021-04-16 03:29:30 -0400

    Table working

--- src/utilities/symbol_table_v2.py
+++ src/utilities/symbol_table_v2.py
@@ -1,14 +1,13 @@
 from typing import List, Dict, Set
-import pdb
 
 class SymbolTable:    
     class Node:
         class Record:
             # Record initialization
-            def __init__(self, symbol: str, symbol_type: str, scope: str):
+            def __init__(self, symbol: str, symbol_type: str, node):
                 self.symbol:        str     = symbol
                 self.symbol_type:   str     = symbol_type
-                self.scope:         str     = scope
+                self.node:          str     = node
             
             # def print_str(self):
             #     out = ''
@@ -27,8 +26,8 @@ class SymbolTable:
             self.parent:    self.Node               = parent
 
         # Node methods
-        def add_record(self, symbol: str, symbol_type: str=None, scope: str=None):
-            self.records[symbol] = self.Record(symbol, symbol_type, scope)
+        def add_record(self, symbol: str, symbol_type: str=None):
+            self.records[symbol] = self.Record(symbol, symbol_type, self)
 
         def add_child(self, scope: str, child):
             self.children[scope] = child
@@ -60,11 +59,15 @@ class SymbolTable:
         return self.scopes[scope]
 
     def print_node(self, out: str, current: Node):
-        out += ' {}\n'.format('-' * 47)
-        out += '| Parent Node: {}\t\t\t\t|\n'.format(current.parent.scope)
+        out += '  Node: {}\n'.format(current.scope)
+        if current.parent != None:
+            out += '  Parent: {}\n'.format(current.parent.scope)
+        
         out += ' {}\n'.format('-' * 47)
         
         # iterate through all records in the current node
+        out += '| Symbol\t\tType\t\tScope\t|\n'
+        out += '| ------\t\t----\t\t-----\t|\n'
         for pair in current.records.items():
             record = pair[1]
 
@@ -72,15 +75,13 @@ class SymbolTable:
             if len(record.symbol) > 5:
                 tabs = 2
 
-            out += '| Symbol\t\tType\t\tScope\t|\n'
-            out += '| ------\t\t----\t\t-----\t|\n'
             out += '| {}{}{}\t\t{}\t|\n'.format(
                 record.symbol,
                 '\t' * tabs,
                 record.symbol_type,
-                record.scope
+                record.node.scope
             )
-            out += ' {}\n'.format('-' * 47)
+        out += ' {}\n\n\n'.format('-' * 47)
         
         return out
 
@@ -88,20 +89,16 @@ class SymbolTable:
         # Perform BFS to print table
         seen: Set = set()
         to_visit = [self.root]
-
-        out = '\n'
+        out = '\n\n'
 
         while len(to_visit) != 0:
             current = to_visit.pop(0)
-            # pdb.set_trace()
-            if not current.scope in seen:
-                # pdb.set_trace()
-                out += self.print_node(out, current)
-                seen.add(current.scope)
-            
+            out = self.print_node(out, current)
+
             for child in current.children.items():
-                # pdb.set_trace()
-                to_visit.append(child[1])
+                if child[1] not in seen:
+                    to_visit.append(child[1])
+                    seen.add(child[1])
 
         return out
 
@@ -111,11 +108,15 @@ s = SymbolTable()
 # Get the root global node in the table
 global_node = s.get_node_by_scope('global')
 
-global_node.add_record('number', None, 'global')
-for_1_node = s.create_node('for_i', global_node)
+global_node.add_record('number', None)
+for_1_node = s.create_node('for_1', global_node)
 if_1_node = s.create_node('if_1', global_node)
 
-for_1_node.add_record('i', None, 'for_1')
-if_1_node.add_record('i', None, 'if_1')
+for_1_node.add_record('i', None)
+if_1_node.add_record('i', None)
+if_1_node.add_record('n', 'Number')
+
+if_2_node = s.create_node('if_2', for_1_node)
+if_2_node.add_record('s', 'String')
 
 print(s)
