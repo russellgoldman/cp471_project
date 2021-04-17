d2ba4bf3a98120258960ca067feaf2872693a879
Zach Merrill <zemerrill@gmail.com>
2021-04-16 23:34:15 -0400

    Finished assemblygen

--- src/assembly/assembly_gen.py
+++ src/assembly/assembly_gen.py
@@ -1,34 +1,19 @@
-import unittest
-import sys, os
-import re
-
-
 def main():
 
-    source_f = open('./given/testcase1.txt', 'r')
+    source_f = open('./given/testcase2.txt', 'r')
     source = source_f.read()
     source_f.close()
 
-    print(codegen(source))
+    f = open('./output_assembly/out2.txt', 'w')
+    f.write(codegen(source))
+    f.close()
 
 def codegen(source):
-    registerCount = 0
     currentRegister = 0 
     assembly = ""
-    lineNumber = 0
-    labelCount = 1
-    label = []
-    variable = []
-    dictionary = {}
     source = source.split('\n')
     for line in source:      
         split = line.split()
-        for word in split:
-            if re.search(r'([A-Za-z]|_)([A-Za-z]|[0-9]|_)*', word):#len(word) > 1:
-                if  word[0] == "L"  and ":" not in word :
-                    label.append(word)
-                elif word != 'goto' and word != 'if' and word not in variable and ":" not in word :
-                    variable.append(word)  
         if "if" in line:
             shift = 0
             if ":" in split[0]:
@@ -36,19 +21,21 @@ def codegen(source):
                 assembly += "{}\n".format(split[0])
             assembly += "LD r{}, {}\n".format(currentRegister,split[1+shift])
             if ">" in line:
-                assembly += "BGT r0, r1,"            #"BGT r0, r1,"  
+                assembly += "LD r{}, {}\n".format(currentRegister+1,split[3+shift])
+                assembly += "BGT r{}, r{}, {}\n".format(currentRegister,currentRegister+1,split[5+shift])           #"BGT r0, r1,"  
             elif "<=" in line:
                 assembly += "LD r{}, {}\n".format(currentRegister+1,split[3+shift])
                 assembly += "BLE r{}, r{}, {}\n".format(currentRegister,currentRegister+1,split[5+shift])            #"BLE r0, r1," 
             elif "<" in line:
-                assembly += "BGT r1, r0,"            #"BGT r1, r0,"
+                assembly += "LD r{}, {}\n".format(currentRegister+1,split[3+shift])
+                assembly += "BGT r{}, r{}, {}\n".format(currentRegister+1,currentRegister,split[5+shift])           #"BGT r1, r0,"
             elif ">=" in line:
-                assembly += "BLE r1, r0,"            #"BLE r1, r0," 
+                assembly += "LD r{}, {}\n".format(currentRegister+1,split[3+shift])
+                assembly += "BLE r{}, r{}, {}\n".format(currentRegister+1,currentRegister,split[5+shift])            #"BLE r1, r0," 
             elif "==" in line:
-                assembly += "BGT r0, r1,"            #"BGT r0, r1,"
-                assembly += "BGT r1, r0,"            #"BGT r1, r0,"    
-            # if "goto" in line:
-            #     assembly += " L1\n"
+                assembly += "LD r{}, {}\n".format(currentRegister+1,split[3+shift])
+                assembly += "BGT r{}, r{}, {}\n".format(currentRegister,currentRegister+1,split[5+shift])            #"BGT r0, r1,"
+                assembly += "BGT r{}, r{}, {}\n".format(currentRegister+1,currentRegister,split[5+shift])            #"BGT r1, r0,"    
         elif "=" in line:
             shift = 0
             if ":" in split[0]:
